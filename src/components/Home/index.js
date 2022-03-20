2f638cef9bad3058b77b75644078090e5fb0e878
maris <marisandb@yahoo.com>
2022-03-20 00:08:57 -0500

    started styling services

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,36 @@ import "./style.css"
 const Home = () =>{
   return (
     <div>
-      <h3>Home Page</h3>
-      <div>
+      <div className='services flex-row'>
+        <div className='services-title'>
+        <h2> Services</h2>
+        </div>
+        <div className='services-list'>
+        <ul>
+          <li> Long Length Acrylics - Starting at $35
+            <br />
+            1 1/2 cm or longer
+          </li>
+          <li> Medium Length Acrylics - Starting at $30
+            <br />
+            7/8 cm or longer
+          </li>
+          <li> Short Length Acrylics - Starting at $25
+            <br />
+            1/2 cm or longer
+          </li>
+          <li> Super Short Length Acrylics - Starting at $20
+            <br />
+            Shorter than 1/2 cm
+          </li>
+          <li>
+            Fills - Starting at 15$
+          </li>
+          <li>
+            Gels Only - Starting at $10
+          </li>
+        </ul>
+        </div>
        
       </div>
     </div>
