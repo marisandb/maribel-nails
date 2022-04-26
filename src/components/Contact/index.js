f3c977a65e224e940292c87b4df6e8dba8b3d770
maris <marisandb@yahoo.com>
2022-04-26 14:41:44 -0500

    added contact form and started mobile first CSS

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 import "./style.css"
 
-const Contact = () =>{
+function Contact() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`A ${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
   return (
-    <div class="contact">
-    <h2>Contact Me</h2>
-    <p>
-      I would love to take care of all of your nail care needs<br />
-      Call or text me and we can get you scheduled right away.
-    </p>
-    <address>
-    <table>
-       <thead>You can reach me at the following:</thead>
-        <tr>
-          <td><i class="fa fa-envelope-o icon" aria-hidden="true"><a href="mailto:maribel.arci@maribelcosmo.com">maribel.arci@maribelcosmo.com</a></i></td>
-          <td><i class="fa fa-phone icon" aria-hidden="true"><a href="tel:608.xxx.xxxx">(608) xxx.xxxx</a></i></td>
-        </tr>
-    </table>
-    </address>
-    <p><b>Please read my policies prior to booking an appointment with me</b><br /></p>
-  </div>
+    <section className='contact-box'>
+      <div className='contact'>
+      <h2>Contact Me</h2>
+        <p>
+        I would love to take care of all of your nail care needs<br />
+        Call or text me and we can get you scheduled right away.
+        </p>
+      </div>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button className="submit-btn" type="submit">Submit</button>
+      </form>
+    </section>
   );
 }
+
 export default Contact;
